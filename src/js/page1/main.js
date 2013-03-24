89a66f1949dd457d98683e5b15b46cc51ea3c24d
sinmetal <metal.tie@gmail.com>
2013-03-24 21:58:45 +0900

    ページごとにミニファイ and 連結したファイルを出力するサンプルを追加 #6

---
+++ src/js/page1/main.js
@@ -0,0 +1,16 @@
+//page1/main.js
+require.config({
+	baseUrl: 'js',
+	paths: {
+		'jquery': 'lib/jquery.js'
+	}
+});
+
+require([
+	'module/hoge',
+	'module/fuga'
+], function(Hoge, Fuga) {
+	// Hogeにはhoge.jsで定義されたコンストラクタが渡る
+	var hoge = new Hoge();
+	hoge.outputLog('Hello Page1 New!');
+});
